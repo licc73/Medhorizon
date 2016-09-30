4a6b7716dbf9837f67af5290c414e5044449219c
changchun li <changchun.li@autodesk.com>
2016-09-30 16:13:59 +0800

    create project and add carthage file.

---
+++ Medhorizon/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Medhorizon
+//
+//  Created by changchun li on 9/30/16.
+//  Copyright © 2016 changchun. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        SignalProducer<Bool, NSError>{ observer, _ in
+            observer.sendNext(true)
+            observer.sendCompleted()
+        }
+        .on { (b) in
+            print(b)
+        }
+        .start()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
