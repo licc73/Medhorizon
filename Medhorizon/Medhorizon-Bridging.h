a9f37c7a38b5b72d62991b5c86f94ead006b744b
ChangchunLi <lccspace@126.com>
2016-10-08 07:26:30 +0800

    third party

---
+++ Medhorizon/Medhorizon-Bridging.h
@@ -0,0 +1,14 @@
+//
+//  Medhorizon-Bridging.h
+//  Medhorizon
+//
+//  Created by lichangchun on 10/7/16.
+//  Copyright © 2016 changchun. All rights reserved.
+//
+
+#ifndef Medhorizon_Bridging_h
+#define Medhorizon_Bridging_h
+
+#import "UIView+Toast.h"
+
+#endif /* Medhorizon_Bridging_h */
