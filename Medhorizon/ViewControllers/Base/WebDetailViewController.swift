daf3a8427cdb4980fe685725ffb3ebce87fedf65
licc73 <licc73@126.com>
2016-10-11 12:50:19 +0800

    js notification

--- Medhorizon/ViewControllers/Base/WebDetailViewController.swift
+++ Medhorizon/ViewControllers/Base/WebDetailViewController.swift
@@ -43,6 +43,17 @@ class WebDetailViewController: UIViewController {
             sPic = brannerData.picUrl
         }
         
+        self.loadWebRequest()
+    }
+    
+    func loadWebRequest() {
+        if let userId = LoginManager.shareInstance.userId {
+            if let link = self.sLink, linkUrl = NSURL(string: link + "&UserId=\(userId)") {
+                self.webView.loadRequest(NSURLRequest(URL: linkUrl))
+                return
+            }
+        }
+        
         if let link = self.sLink, linkUrl = NSURL(string: link) {
             self.webView.loadRequest(NSURLRequest(URL: linkUrl))
         }
@@ -63,13 +74,31 @@ class WebDetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func notifyWebViewUserLogin(userId: String) {
+        self.webView.stringByEvaluatingJavaScriptFromString(String("setUserId(\(userId));"))
+    }
+    
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        let requestString = request.URL?.absoluteString
+        if let js = requestString where js == "http://app.medhorizon.com.cn/NewsDetail/call-medicine_app-method://login" {
+            LoginManager.loginOrEnterUserInfo()
+            return false
+        }
+        else if let js = requestString where js == "http://app.medhorizon.com.cn/NewsDetail/call-medicine_app-method://share" {
+            return false
+        }
         return true
     }
 }
 
 extension WebDetailViewController {
     @IBAction func back(sender: AnyObject) {
-        self.navigationController?.popViewControllerAnimated(true)
+        if self.webView.canGoBack {
+            self.webView.goBack()
+        }
+        else {
+            self.navigationController?.popViewControllerAnimated(true)
+        }
     }
 }
